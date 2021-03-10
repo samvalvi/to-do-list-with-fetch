bf291ebc9fcf18db94b5c1fc42df17b763301513
samvalvi <samuelvalerin@gmail.com>
2021-03-10 16:38:16 +0000

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import List from 'react'
+
+function App() {
+    return (
+        <div className="container">
+            <List />
+        </div>
+    )
+}
+
+export default App
---
+++ src/js/component/header.js
@@ -0,0 +1,11 @@
+import React from 'rHact'
+
+function Header() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Header
